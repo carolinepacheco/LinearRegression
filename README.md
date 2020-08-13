6f88cbdd774202ee583506755abff3fe39b0fa15
Caroline Pacheco <lolyne.pacheco@gmail.com>
2020-08-13 22:55:57 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # LinearRegression
+
+Last Page Update: **13/08/2020**
+
+
 A python implementation of linear regression written from scratch
+
+
+Linear Regression
+---------------------------------------------------
+
+Linear Regression is one of the most popular supervised machine learning. It predicts values within a continuous range, (e.g. sale prices, life expectancy, temperature, etc) instead of trying to classify them into categories (e.g. car, bus, bike and others). The main goal of the linear regression is to find the best fit line which describes the relationship between the data.
+
+<p align="center"><img src="https://raw.githubusercontent.com/carolinepacheco/LinearRegression/master/docs/line_fit.gif" border="0" /></p>
+
+
+The best-fitting line (or regression line) is represented by:
+
+
+<p align="center"><img src="https://raw.githubusercontent.com/carolinepacheco/LinearRegression/master/docs/line.png" border="0" /></p>
+
+where m is the slope of the line known as angular gradient and b is the point at which the line crosses the y-axis also known as linear gradient.
+
+
+You can find more details about Linear Regression at https://medium.com/p/dee84865cbf7.
---
+++ linear_regression.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Nome: Caroline Pacheco do E. Silva
+E-mail: lolyne.oacheco@gmail.com
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+from sklearn.datasets import make_regression
+
+# Generate a random regression data
+X, y = make_regression(n_samples=120, n_features=1, noise=0.4, bias=50)
+
+# The equation of a straight line is represented by: y = mx + b. 
+# The (m, b) are two variables that can updat.
+def hypothesis(m, b, x):
+    return m + (b*x) 
+
+# Display the graph ith the line 
+def plot_line(m, b, X, y):
+    max_x = np.max(X) + 100
+    min_x = np.min(X) - 100
+
+    xplot = np.linspace(min_x, max_x, 1000)
+    yplot = m + b * xplot
+
+    plt.plot(xplot, yplot, color='#ff0000', label='Regression Line')
+
+    plt.scatter(X,y)
+    plt.axis([-10, 10, 0, 200])
+    plt.show()
+
+# Initially, the values of a and b receive random values to begin with. 
+m = np.random.rand()
+b = np.random.rand()
+
+# Display a line with randomly initialized m and b values.
+plot_line(m, b, X, y)
+
+# The line drawn in the graph above is wrong. 
+# Therefore, we will calculate the total error of the line using the Mean Squared Error(MSE) function.
+
+# Calculate the error function.
+def cost(m, b, X, y):
+    costValue = 0 
+    for (xi, yi) in zip(X, y):
+        costValue += 0.5 * ((hypothesis(m, b, xi) - yi)**2)
+    return costValue
+
+# Now we need to adjust the function to reduce this error.
+# Calculate the derivatives using the following function.
+def derivatives(m, b, X, y):
+    dm = 0
+    db = 0
+    for (xi, yi) in zip(X, y):
+        dm += hypothesis(m, b, xi) - yi
+        db += (hypothesis(m, b, xi) - yi)*xi
+
+    dm /= len(X)
+    db /= len(X)
+
+    return dm, db
+
+# Using  the gradient update rule  to update the (m, b) parameters .    
+def update_parameters(m, b, X, y, alpha):
+    dm, db = derivatives(m, b, X, y)
+    m = m - (alpha * dm)
+    b = b - (alpha * db)
+
+    return m, b
+
+
+# Now we repeat these steps - checking the error, calculating the derivatives,
+# and updating the weights until the error is as low as possible.
+# This is called minimizing the cost function.
+
+# Mnimizing the cost function    
+def linear_regression(X, y):
+    m = np.random.rand()
+    b = np.random.rand()
+
+    for i in range(0, 1000):
+        if i % 100 == 0:
+            plot_line(m, b, X, y)
+        m, b = update_parameters(m, b, X, y, 0.005)
+
+
+# call function
+linear_regression(X, y)
